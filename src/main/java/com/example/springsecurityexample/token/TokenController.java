73f5b590968d31a8046bcb098c819477757e5994
vinicius <vinc-analist@hotmail.com>
2022-06-11 11:24:24 -0300

    initial commit

---
+++ src/main/java/com/example/springsecurityexample/token/TokenController.java
@@ -0,0 +1,43 @@
+package com.example.springsecurityexample.token;
+
+import com.nimbusds.jwt.JWTClaimsSet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+@RestController
+public class TokenController {
+
+    @Autowired
+    JwtEncoder encoder;
+
+    @PostMapping("/token")
+    public String token(Authentication authentication) {
+        // TODO: 11/06/22 implement tests 
+        Instant now = Instant.now();
+        long expiry = 36000l;
+
+        String scope = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiry))
+                .subject(authentication.getName())
+                .claim("subject", scope)
+                .build();
+
+        return this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+}
+
